4f1065df2e268c5703f2e12b88931f2d354cb612
saaraorav5366 <so24@rice.edu>
2024-03-13 16:33:16 -0500

    Initial commit

---
+++ src/main/java/com/example/CGI/suvepraktika/CgiSuvepraktikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.CGI.suvepraktika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiSuvepraktikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiSuvepraktikaApplication.class, args);
+	}
+
+}
